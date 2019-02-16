df6d3cfa234c9099f2686522873324359abe351d
Dowglas Maia <dowglasmaia@gmail.com>
2019-02-16 18:07:30 -0200

    Definindo o layout do Sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/* Modulos do PrimeNG - https://www.primefaces.org/primeng/#/ */
+import {ToolbarModule} from 'primeng/toolbar';
+import {AccordionModule} from 'primeng/accordion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
+
+  /* Declara os Imports  no imports: []*/
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToolbarModule,
+    AccordionModule,
+    BrowserAnimationsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
