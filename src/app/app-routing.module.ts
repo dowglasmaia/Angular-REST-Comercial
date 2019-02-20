e1f427e2e32507af608cef03d0c4531bf59c39ab
Dowglas Maia <dowglasmaia@gmail.com>
2019-02-20 15:24:44 -0300

    Iniciando Implementação de Guarda de Rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { HomeComponent } from './components/home/home.component';
 /**
  * @author Dowglas maia
  * @email dowglasmaia@live.com
@@ -14,14 +13,16 @@ import { CommonModule } from '@angular/common';
 import { CargoComponent } from './components/cadastros/cargo/cargo.component';
 import { CargoDetalheComponent } from './components/cadastros/cargo/cargo-detalhe/cargo-detalhe.component';
 import { NaoEncontradoComponent } from './components/nao-encontrado/nao-encontrado.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 /* Definindo as Rotas, da Aplicação - (1:path(url), - 2:componente )*/
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'cargo', component: CargoComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
+  { path: 'login', component: LoginComponent },
+  { path: 'cargo', component: CargoComponent},
   { path: 'cargo/novo', component: CargoDetalheComponent },
   { path: 'cargo/:id', component: CargoDetalheComponent },
   { path: 'nao-encontrado', component: NaoEncontradoComponent },
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,29 @@
+/*
+Class para Guarda de Rotas, fazendo um filtro na requisiçao da mesma.
+*/
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { LoginService } from './login/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private loginService: LoginService,
+    private router: Router) { }
+
+  /* Metodo sera Chamando sempre que o Usuario fizer Alguma Rota, autorizando ou negando a mesma! */
+  canActivate(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+
+
+    if (this.loginService.usuarioLogado) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
